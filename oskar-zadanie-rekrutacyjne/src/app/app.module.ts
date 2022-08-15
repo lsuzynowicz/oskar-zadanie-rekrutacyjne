af4880c2d99be392cfae78c01f30fc07cb27423c
Łukasz Suzynowicz <l.suzynowicz@gmail.com>
2022-08-15 22:19:41 +0200

    modify app.module file

--- oskar-zadanie-rekrutacyjne/src/app/app.module.ts
+++ oskar-zadanie-rekrutacyjne/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { httpInterceptorProviders } from './_helpers/auth.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,8 +15,9 @@ import { httpInterceptorProviders } from './_helpers/auth.interceptor';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
